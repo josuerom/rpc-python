39951f059b0581dd00021a2c2f5932ab928f08ae
Josue Romero <josueromram3@gmail.com>
2023-05-03 03:32:15 +0000

    RPC PYTHON

--- clientNC.py
+++ clientNC.py
@@ -2,5 +2,5 @@ import xmlrpc.client
 
 s = xmlrpc.client.ServerProxy('http://172.19.0.4:8002') # Manual connection to node 2
 result = s.node()
-print("HIMNO A LA ALEGRÍA DE LUDWING VAN BEETHOVEN:")
+print("HIMNO A LA ALEGRÍA DE LUDWING VAN BEETHOVEN:\n")
 print(str(result))
